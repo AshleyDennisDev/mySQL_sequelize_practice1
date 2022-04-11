e50056e8a746d9845b64cdf303d7c05f3a1a7bcb
Ashley Dennis <adenn1852@gmail.com>
2022-04-11 16:26:24 -0400

    able to post and get users

--- server/routes/user.js
+++ server/routes/user.js
@@ -4,27 +4,15 @@ const router = express.Router()
 const {User} = require("../models");
 
 router.get("/", async (req, res) => {
-    const user = User.findAll();
+    const user = await User.findAll();
     res.json(user);
 });
 
-router.post("/"), async (req, res) => {
+router.post("/", async (req, res) => {
     const values = req.body;
     const createdUser = await User.create(values);
     res.json(createdUser);
-}
-// router.get("/insert", (req,res) => {
-//     User.create({
-//         firstName:"Emma",
-//         lastName:"Richards",
-//         age: 4,
-//     })
-//     .catch((err) => {
-//         if (err) {
-//             console.log(err);
-//         }
-//     })
-//     res.send("insert");
-// })
+})
+
 
 module.exports = router;
